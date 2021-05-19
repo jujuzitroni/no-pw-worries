f048ac99d2d7fd1a2b7e86839af9d82a4d7ee056
jujuzitroni <jujuzitroni@gmail.com>
2021-05-19 14:04:37 +0200

    :sparkles: Connect to database

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,17 @@
-import express, { response } from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+import { connectDatabase } from './utils/database';
+
+if (process.env.MONGO_URL === undefined) {
+  throw new Error('Missing env MONGO_URL');
+}
 
 const app = express();
 const port = 5000;
 
-app.get('/api/credentials', (_request, respone) => {
+app.get('/api/credentials', (_request, response) => {
   response.send('All credentials requested');
 });
 
@@ -11,6 +19,9 @@ app.post('/api/credentials', (_request, response) => {
   response.send('Add new credential');
 });
 
-app.listen(port, () => {
-  console.log(`nopwworries listening at http://localhost:&{port}`);
+connectDatabase(process.env.MONGO_URL).then(() => {
+  console.log('Database connected');
+  app.listen(port, () => {
+    console.log(`nopwworries listening at http://localhost:${port}`);
+  });
 });
