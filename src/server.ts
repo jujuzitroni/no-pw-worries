bec25af2b4cefaece4a2832f73cc9234cc83d062
jujuzitroni <jujuzitroni@gmail.com>
2021-05-19 11:34:22 +0200

    :heavy_plus_sign: Add express server

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,16 @@
-console.log('server is running');
+import express, { response } from 'express';
 
-export {};
+const app = express();
+const port = 5000;
+
+app.get('/api/credentials', (_request, respone) => {
+  response.send('All credentials requested');
+});
+
+app.post('/api/credentials', (_request, response) => {
+  response.send('Add new credential');
+});
+
+app.listen(port, () => {
+  console.log(`nopwworries listening at http://localhost:&{port}`);
+});
